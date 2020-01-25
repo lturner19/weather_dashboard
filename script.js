296433d2c01488fd6577f4926419350c36ffe26a
Leandra Turner <calliet0715@gmail.com>
2020-01-24 19:52:16 -0600

    Initial code setup

---
+++ script.js
@@ -0,0 +1,14 @@
+const apiKey = "&appid=1b5bd969040dfbf14940a6cc061c29a4";
+const city = [];
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + apiKey;
+const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + apiKey;
+const uviURL = "http://api.openweathermap.org/data/2.5/uvi/forecast?appid={appid}&lat={lat}&lon={lon}&cnt={cnt}"
+apiKey;
+let currentDate = moment().format('LL');
+
+
+
+
+$("button").on("click", function () {
+
+})
