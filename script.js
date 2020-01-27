e77361241b594facd2bb9ddcb9590d7f0ee49c0f
Leandra Turner <calliet0715@gmail.com>
2020-01-26 21:40:59 -0600

    For loop coding for city array added

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const apiKey = "appid=1b5bd969040dfbf14940a6cc061c29a4";
 let city = [];
+let index = 0;
 let weatherURL = "https://api.openweathermap.org/data/2.5/weather?" + apiKey + "&q=";
 let forecastURL = "http://api.openweathermap.org/data/2.5/forecast?" + apiKey + "&q=";
 let uviURL = "http://api.openweathermap.org/data/2.5/uvi?" + apiKey;
 
 
-/* <header>
-<h1>Weather Dashboard</h1>
-</header>
-<div class="container">
-<div class="row" id="weather-info">
-    <div class="col-sm-4">
-        <div class="card" style="width: 80%;">
-            <div class="card-body" id="search">
-                <h5>Search for City:</h5>
-                <input label="search" id="user-input" />
-                <button><i class="fas fa-search"></i></button>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-8">
-        <div class="card" style="width: 100%;">
-            <div class="card-body" id="city-info">
 
-            </div>
-        </div>
-        <div id="five-day"></div>
-    </div>
+$(document).on("click", ".listnone", displayWeather);
 
-</div>
-</div>
-
-<footer>
-Created by: lturner19
-</footer>
- */
+var storedCities = localStorage.getItem("city");
+if (storedCities !== null) {
+    var cityArr = JSON.parse(storedCities);
+    if (cityArr.length >= 0) {
+        for (index = 0; index < cityArr.length; index++) {
+            var ul = $("<ul>").attr("class", "listnone");
+            var li = $("<li>");
+            li.append(cityArr[index]);
+            ul.append(li);
+            $("#user-input").append(ul);
+        }
+    }
+}
 
 
 $("button").on("click", function () {
     const cityInput = $("#user-input").val().trim(); //grabs the user's city input
+    city.push(cityInput);
+    let storeCities = localStorage.setItem("city", JSON.stringify(city));
     displayWeather(cityInput);
-    //can put validation here to handle when user does not put a response
+
+    if (cityInput === "") {
+        alert("Please enter a city name.");
+    }
+    var ul = $("<ul>").attr("class", "listnone");
+    var li = $("<li>");
+    li.append(cityInput);
+    ul.append(li);
+    $(".cityList").prepend(ul);
+    localStorage.getItem("cityArr", cityArr);
+
+    weatherURL = weatherURL.concat(city)
 });
 
 function displayWeather(city) {
@@ -53,11 +53,10 @@ function displayWeather(city) {
     }).then(function (response) {
 
         var cityHeading = $("<h3 class='user-city'>");
-
         cityHeading.append(response.name);
 
         var date = $("<span>")
-        date.html(" " + moment(response.dt, "X").format("MM/DD/YYYY"));
+        date.html(" " + moment(response.dt, "X").format("MM/DD/YYYY"));//converting the dt from a number to a string, then back to a number
         cityHeading.append(date);
 
         const pOne = $("<p class='temperature'>");
@@ -71,12 +70,10 @@ function displayWeather(city) {
         const pThree = $("<p class='wind-speed'>");
         pThree.html("Wind Speed:   " + response.wind.speed + "mph");
 
-        //put 2nd call back for uv w/in this function
-
-
 
         forecastURL = forecastURL.concat(city);
 
+        //calling forecast api to get coordinates for uv index
         $.ajax({
             url: forecastURL,
             method: "GET"
@@ -84,53 +81,53 @@ function displayWeather(city) {
 
             let longitude = data.city.coord.lon
             let latitude = data.city.coord.lat
-            var row =$("<div class='row'>");
+            var row = $("<div class='row' id='second-row'>");
             var colOne = $("<div class='col1'>");
-        //colOne.html(data.)
-        var colTwo;
-        var colThree;
-        var colFour;
-        var colFive;
+
+            //empty variables to hold 5 day forecast info from api list array
+            var colTwo;
+            var colThree;
+            var colFour;
+            var colFive;
 
 
-for (let index = 0; index < data.list.length; index++) {
+            for (let index = 1; index < data.list.length; index++) {//array from forecast api
 
-       if(data.list[index].dt_txt.indexOf("00:00:00") > -1 ){
-        colOne = $("<div class='col-sm-2'>");
-        colOne.append(moment(data.list[index].dt,"X").format("MM/DD/YYYY"));
-        var img = "<img src='http://openweathermap.org/img/w/"+ data.list[index].weather[0].icon+  ".png' >"
-       colOne.append(img)
-       
-       var p = $("<p class='ptemp'>")
-       p.html("Temperature   " +data.list[index].main.temp)
+                if (data.list[index].dt_txt.indexOf("00:00:00") > -1) {//grabs the weather info at midnight for each of the 5 days
+                    colOne = $("<div class='col-sm-2'>"); //keeps the columns in a row
+                    colOne.append(moment(data.list[index].dt, "X").format("MM/DD/YYYY"));
+                    var img = "<img src='http://openweathermap.org/img/w/" + data.list[index].weather[0].icon + ".png' >"
 
-       colOne.append(p);
-        row.append(colOne);       
-       }
-    }
-         
-       $("#five-day").append(row);  
-         
+                    var pTagOne = $("<p class='ptemp'>")
+                    let tempConversion = (Math.floor((data.list[index].main.temp - 273.15) * 1.80) + 32);
+                    pTagOne.html("Temp:   " + tempConversion + "°F");
+
+                    var pTagTwo = $("<p class='humid'>");
+                    pTagTwo.html("Humidity:   " + data.list[index].main.humidity + " %");
+                    colOne.append(img, pTagOne, pTagTwo);
+                    row.append(colOne);
+                }
+            }
+            $("#five-day").append(row);
+
+            //Calling uv index with long. and lat. coordinates
             $.ajax({
                 url: uviURL + "&lat=" + latitude + "&lon=" + longitude,
                 method: "GET"
             }).then(function (data) {
 
                 var pFour = $("<p class='uvi'>");
-
-                pFour.html("UV Index   " + data.value);
-
+                if (data.value <= 3) {
+                    pFour = $("<p class='ok'>");
+                } else if (data.value > 3 && data.value < 7) {
+                    pFour = $("<p class='warning'>");
+                } else {
+                    pFour = $("<p class='danger'>");
+                }
+                pFour.append("UV Index:   ")
+                pFour.append(data.value);
                 $("#city-info").append(cityHeading, pOne, pTwo, pThree, pFour)
-                
-              
-
             })
-
         })
-
-        //need to display the forecast for the next 5 days
-        //need dates, temperature, icon, humidity
-
     });
-
 }
